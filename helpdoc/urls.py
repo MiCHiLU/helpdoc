d95d277d5361526d88bbbd224fc82859df0cbf52
endoh <endoh@d16649c9-553b-0410-a106-67708380cd12>
2007-06-16 04:08:09 +0000

    r2261@PB12:  endoh | 2007-06-10 17:49:04 +0900  Fixed: michilu, helpdoc. auth.

--- helpdoc/urls.py
+++ helpdoc/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
-from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import permission_required
 from views import app_labels, render
 
-app_labels = staff_member_required(app_labels)
+app_labels = permission_required("is_staff")(app_labels)
 
 
 urlpatterns = patterns('',)
