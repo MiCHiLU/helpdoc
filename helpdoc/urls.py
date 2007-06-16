29640ae73eba5adca1315612c134fb606492141f
endoh <endoh@d16649c9-553b-0410-a106-67708380cd12>
2007-06-16 04:08:00 +0000

    r2259@PB12:  endoh | 2007-06-10 11:06:44 +0900  Updated: michilu, helpdoc. added auth require.

--- helpdoc/urls.py
+++ helpdoc/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
+from django.contrib.admin.views.decorators import staff_member_required
 from views import app_labels, render
+
+app_labels = staff_member_required(app_labels)
 
 
 urlpatterns = patterns('',)
