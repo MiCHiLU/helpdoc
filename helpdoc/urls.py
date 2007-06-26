ab5d390409cf5e8dc2432989dd97b4d5905b52b7
endoh <endoh@d16649c9-553b-0410-a106-67708380cd12>
2007-06-26 10:09:39 +0000

    Refactored: michilu, helpdoc.

--- helpdoc/urls.py
+++ helpdoc/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import permission_required
-from django.views.generic.simple import redirect_to
-from views import app_labels, render
+from django.views.generic.simple import direct_to_template, redirect_to
+from views import render
 
-app_labels = permission_required("is_staff")(app_labels)
+direct_to_template = permission_required("is_staff")(direct_to_template)
 render = permission_required("is_staff")(render)
 
 urlpatterns = patterns('',)
 
 urlpatterns += patterns('',
-    (r'^$', app_labels),
+    (r'^$', direct_to_template, {"template":"helpdoc/index.html"}),
     (r'^(?P<app>\w+)/(?P<doc>[0-9a-z-_\.]+)/$', render),
     (r'^(?P<app>\w+)/', redirect_to, {'url' : "index/"}),
 )
