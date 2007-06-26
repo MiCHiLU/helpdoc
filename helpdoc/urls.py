19ba75750c704a09e3a48f284d69ad2b6db11411
endoh <endoh@d16649c9-553b-0410-a106-67708380cd12>
2007-06-26 10:09:31 +0000

    Refactored: michilu, helpdoc.

--- helpdoc/urls.py
+++ helpdoc/urls.py
@@ -3,17 +3,12 @@ from django.contrib.auth.decorators import permission_required
 from django.views.generic.simple import redirect_to
 from views import app_labels, render
 
-option = dict(login_url="/helpdoc/accounts/login/")
-
-app_labels = permission_required("is_staff", **option)(app_labels)
-render = permission_required("is_staff", **option)(render)
-
+app_labels = permission_required("is_staff")(app_labels)
+render = permission_required("is_staff")(render)
 
 urlpatterns = patterns('',)
 
 urlpatterns += patterns('',
-    (r'^accounts/login/$', 'django.contrib.auth.views.login', {"template_name":"admin/login.html"}),
-
     (r'^$', app_labels),
     (r'^(?P<app>\w+)/(?P<doc>[0-9a-z-_\.]+)/$', render),
     (r'^(?P<app>\w+)/', redirect_to, {'url' : "index/"}),
